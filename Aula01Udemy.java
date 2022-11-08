3be6cfe31c4f36c557bf03b601341c409ea94206
André De Campos <andre.olegario.campos@gmail.com>
2022-11-08 10:40:40 -0300

    Add files via upload

---
+++ Aula01Udemy.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula01udemy;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Aula01Udemy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primeiro programa aula 01 da udemy de java do basico ao avançado!");
+        System.out.println("Para da sorte não pode faltar Hello world!");
+    }
+    
+}
