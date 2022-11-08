64643ee016e9f4948ce74a467a05abda5caab6d1
André De Campos <andre.olegario.campos@gmail.com>
2022-11-08 11:21:22 -0300

    Add files via upload

---
+++ Agenda.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FinalidadeDaAula.Administracao;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Agenda {
+    
+}
---
+++ Finalidade.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package FinalidadeDaAula;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Finalidade {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("A finalidade da aula e aprender a organizar,"
+                + " criar pacotes e classes com finalidade de uma interagir com a outra futuramente");
+    }
+    
+}
